a8e71d01c9031a9533768169d3ec4caa1564fb96
Er-sumit <martini.chaise_0d@icloud.com>
2024-03-27 14:09:49 +0530

    CURD APIs for blog

--- apis/base.py
+++ apis/base.py
@@ -2,9 +2,11 @@
 from fastapi import APIRouter
 
 from apis.v1 import route_user
+from apis.v1 import route_blog
 
 
 api_router = APIRouter()
 api_router.include_router(route_user.router,prefix="",tags=["users"])
+api_router.include_router(route_blog.router,prefix="",tags=["blogs"])
 
 # make sure we import this 'api_router' in the main.py file
